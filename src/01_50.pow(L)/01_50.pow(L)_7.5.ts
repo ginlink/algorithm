13f71c324851ab1125a6c466a246ec6d10070541
ginlink <ginlink1998@gmail.com>
2022-07-05 08:01:31 +0800

    review: 7.5

---
+++ src/01_50.pow(L)/01_50.pow(L)_7.5.ts
@@ -0,0 +1,18 @@
+// 题目：实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+// 输入: 2.0, 10;
+// 输出: 1024.0;
+
+// 难度：简单
+
+export function quickMul(x: number, n: number): number {
+  if (n == 0) {
+    return 1;
+  }
+
+  const y = quickMul(x, Math.floor(n / 2));
+
+  // 为什么如果冥是奇数这里要乘以 底？
+  // 因为 要抵消向下取整的差值
+  return n % 2 == 0 ? y * y : y * y * x;
+}
---
+++ src/02_155最小栈(L)/02_155最小栈(L)_7.5.ts
@@ -0,0 +1,36 @@
+// 题目：155.最小栈
+// 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+// push(x) —— 将元素 x 推入栈中。 pop() —— 删除栈顶的元素。 top() —— 获取栈顶元素。 getMin() —— 检索栈中的最小元素。
+
+// 输入
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [],[-2],[0],[-3],[],[],[],[]
+
+// 输出
+// ;[null, null, null, null, -3, null, 0, -2]
+
+// 难度：简单
+// [1] https://segmentfault.com/a/1190000041534341
+
+export class MinStack {
+  private stack: number[] = [];
+  private minStack: number[] = [Infinity];
+
+  push(e: number) {
+    this.stack.push(e);
+    this.minStack.push(Math.min(e, this.minStack[this.minStack.length - 1]));
+  }
+
+  pop() {
+    this.minStack.pop();
+    return this.stack.pop();
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
