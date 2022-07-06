5dd2862f427db64aaf68b1ffddbbda8d70f85fd4
ginlink <ginlink1998@gmail.com>
2022-07-06 23:35:39 +0800

    chore: fix eslint

--- src/03_022.102二叉树的层序遍历(L)/03_022.102二叉树的层序遍历(L)_7.6.ts
+++ src/03_022.102二叉树的层序遍历(L)/03_022.102二叉树的层序遍历(L)_7.6.ts
@@ -2,34 +2,34 @@
 // 输入：root = [3,9,20,null,null,15,7]
 // 输出：[[3],[9,20],[15,7]]
 
-import { TreeNode } from '../utils';
+import { TreeNode } from '../utils'
 
 export function levelOrder(root: TreeNode | null): number[][] {
-  const queue: (TreeNode | null)[] = [];
-  const result: number[][] = [];
+  const queue: (TreeNode | null)[] = []
+  const result: number[][] = []
 
-  queue.unshift(root);
+  queue.unshift(root)
 
   while (queue.length > 0) {
-    const len = queue.length;
-    const layer: number[] = [];
+    const len = queue.length
+    const layer: number[] = []
     for (let i = 0; i < len; ++i) {
-      const el = queue.pop();
+      const el = queue.pop()
 
       if (el?.val) {
-        layer.push(el.val);
+        layer.push(el.val)
       }
 
       if (el?.left) {
-        queue.unshift(el.left);
+        queue.unshift(el.left)
       }
       if (el?.right) {
-        queue.unshift(el.right);
+        queue.unshift(el.right)
       }
     }
 
-    result.push(layer);
+    result.push(layer)
   }
 
-  return result;
+  return result
 }
