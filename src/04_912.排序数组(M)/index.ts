522f8eee310e5dd1f3a2d0450d0c6c25672cdfe7
ginlink <ginlink@163.com>
2022-08-06 09:27:34 +0800

    review: 8.6

--- src/04_912.排序数组(M)/index.ts
+++ src/04_912.排序数组(M)/index.ts
@@ -2,4 +2,5 @@
 // export * from './arraySort_7.8'
 // export * from './arraySort_7.9'
 // export * from './arraySort_7.12'
-export * from './arraySort_8.1'
+// export * from './arraySort_8.1'
+export * from './arraySort_8.6'
