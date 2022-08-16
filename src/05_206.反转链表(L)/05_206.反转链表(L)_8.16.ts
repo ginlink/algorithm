cd58fe0a6e69f4e660ae11ac2cac1b5030859269
ginlink <ginlink1998@gmail.com>
2022-08-16 10:05:44 +0800

    review 206 reverse list node

---
+++ src/05_206.反转链表(L)/05_206.反转链表(L)_8.16.ts
@@ -0,0 +1,32 @@
+import { ListNode } from '../utils'
+
+export function reverseListByRec(root: ListNode | undefined) {
+  return root ? recReverseList(root) : undefined
+
+  function recReverseList(node: ListNode): ListNode {
+    if (!node.next) {
+      return node
+    }
+
+    const head = recReverseList(node.next)
+    node.next.next = node
+    node.next = undefined
+
+    return head
+  }
+}
+
+export function reverseListByLoop(root: ListNode | undefined): ListNode | undefined {
+  let pre = undefined,
+    curr = root
+
+  while (curr) {
+    const next = curr.next
+
+    curr.next = pre
+    pre = curr
+    curr = next
+  }
+
+  return pre
+}
